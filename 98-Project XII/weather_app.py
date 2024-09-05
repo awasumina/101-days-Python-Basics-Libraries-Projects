b80baab844280b37ceafd113b84d7ece99ebd5b6
sumina <suminaawa123@gmail.com>
2024-09-05 21:52:47 +0545

    Weather App

---
+++ 98-Project XII/weather_app.py
@@ -0,0 +1,15 @@
+# pip install bs4
+
+import requests
+from bs4 import BeautifulSoup as soup
+
+# search  = "Weather in Bhaktapur, Nepal"
+search = input("Please provide the city name where you want to know the weather as 'Weather In Tokyo:\n'")
+url = f"https://www.google.com/search?&q={search}" # this indicates the search paramter in the given url
+
+response=requests.get(url)   # get method to fetch data from a website and store it into response object
+
+s = soup(response.text,"html.parser") # This html parses/fetches the data from url
+
+update = s.find("div", class_ = "BNeawe").text # built in class
+print (update)
